fe77872dd0098b9f57ffab3ba14109dfd7f24b08
maxwen <max.weninger@gmail.com>
2018-09-09 22:11:07 +0200

    base: SystemUI: add qs quick tile column auto to follow columns
    always use same columns as the expanded panel but
    never less then 6
    
    Change-Id: I13f5f871cc1362d1fb8c60ba07aeff9675f1c32a

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -728,4 +728,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             }
         }
     }
+
+    public int getNumColumns() {
+        return mTileLayout.getNumColumns();
+    }
 }
