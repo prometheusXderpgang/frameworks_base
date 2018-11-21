17a17ca3b6e38b4545f1f8a2440cc6e94f0711f1
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice
    Pie Bringup by ZeNiXxX

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5729,6 +5729,20 @@ public final class Settings {
             return value;
         }
 
+        	    /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+        
+	    /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
+
         /**
          * Convenience function for retrieving a single secure settings value
          * as a {@code long}.  Note that internally setting values are always
