d12f1c61b2663246128360559734770de4c59024
ZyxxeZ <bankersenator@gmail.com>
2018-11-24 12:32:11 +0000

    small fix

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -94,7 +94,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_PINNING_TOAST_ENTER_EXIT = 45 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ESCAPE     = 46 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 47 << MSG_SHIFT;
-    private static final int MSG_RESTART_UI                    = 47 << MSG_SHIFT;
+    private static final int MSG_RESTART_UI                    = 48 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -559,6 +559,8 @@ public class CommandQueue extends IStatusBar.Stub {
         synchronized (mLock) {
             mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
             mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
+        }
+    }
 
     public void restartUI() {
         synchronized (mLock) {
@@ -816,6 +818,8 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
+                    }
+                    break;
                 case MSG_RESTART_UI:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).restartUI();
